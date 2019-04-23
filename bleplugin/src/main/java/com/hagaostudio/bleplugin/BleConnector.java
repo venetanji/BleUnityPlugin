4debc70f446b9924fb41cdc18f0ef25ddfabad97
Giovanni Lion <giovanni.lion@gmail.com>
2019-04-23 19:03:54 +0800

    fuck yeah

--- bleplugin/src/main/java/com/hagaostudio/bleplugin/BleConnector.java
+++ bleplugin/src/main/java/com/hagaostudio/bleplugin/BleConnector.java
@@ -2,6 +2,8 @@ package com.hagaostudio.bleplugin;
 
 import android.bluetooth.*;
 import android.util.Log;
+
+import java.nio.charset.StandardCharsets;
 import java.util.Set;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.app.ActivityCompat;
@@ -10,14 +12,24 @@ import android.content.Context;
 import android.app.Activity;
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.util.Xml;
 
 
 public class BleConnector {
     private final static String TAG = "BleUnity";
-    public static final String SerialProtUUID="0000dfb1-0000-1000-8000-00805f9b34fb";
+    public static final String SerialPortUUID="0000dfb1-0000-1000-8000-00805f9b34fb";
+    public static final String CommandUUID="0000dfb2-0000-1000-8000-00805f9b34fb";
+    private BluetoothGattCharacteristic serialChar;
+    private BluetoothGattCharacteristic commandChar;
+
     private int connectionState = STATE_DISCONNECTED;
     private Context appContext;
     private Activity appActivity;
+    public String incomingData;
+
+    private int mBaudrate=115200;	//set the default baud rate to 115200
+    private String mPassword="AT+PASSWOR=DFRobot\r\n";
+    private String mBaudrateBuffer = "AT+CURRUART="+mBaudrate+"\r\n";
 
     private static final int STATE_DISCONNECTED = 0;
     private static final int STATE_CONNECTING = 1;
@@ -62,7 +74,15 @@ public class BleConnector {
             if (status == BluetoothGatt.GATT_SUCCESS) {
                 for (BluetoothGattService gattService : gatt.getServices()) {
                     for (BluetoothGattCharacteristic gattCharacteristic : gattService.getCharacteristics()) {
-                        Log.i(TAG, "Characteristic discovered:  " + gattCharacteristic.getUuid().toString());
+                        if (gattCharacteristic.getUuid().toString().equals(SerialPortUUID)) {
+                            Log.i(TAG, "SerialPort char found:  " + gattCharacteristic.getUuid().toString());
+                            serialChar = gattCharacteristic;
+                        }
+
+                        if (gattCharacteristic.getUuid().toString().equals(CommandUUID)) {
+                            Log.i(TAG, "SerialPort command found:  " + gattCharacteristic.getUuid().toString());
+                            commandChar = gattCharacteristic;
+                        }
                     }
                     Log.i(TAG, "onServicesDiscovered received: " + gattService.getUuid().toString());
                 }
@@ -71,12 +91,31 @@ public class BleConnector {
 
         }
 
+//        @Override
+//        // Result of a characteristic read operation
+//        public void onCharacteristicRead(BluetoothGatt gatt,
+//                                         BluetoothGattCharacteristic characteristic,
+//                                         int status) {
+//            final byte[] data = characteristic.getValue();
+//            if (data != null && data.length > 0) {
+//                Log.i(TAG, "onCharacteristic read1: " + new String(data, StandardCharsets.UTF_8));
+//                Log.i(TAG, "onCharacteristic read2: " + new String(data, StandardCharsets.US_ASCII));
+//                Log.i(TAG, "onCharacteristic read3: " + new String(data, StandardCharsets.UTF_16));
+//                Log.i(TAG, "onCharacteristic read4: " + new String(data, StandardCharsets.UTF_16BE));
+//                Log.i(TAG, "onCharacteristic read5: " + new String(data, StandardCharsets.UTF_16LE));
+//                Log.i(TAG, "onCharacteristic read6: " + data.toString());
+//            }
+//
+//            //bluetoothGatt.readCharacteristic(serialChar);
+//        }
+
+
         @Override
-        // Result of a characteristic read operation
-        public void onCharacteristicRead(BluetoothGatt gatt,
-                                         BluetoothGattCharacteristic characteristic,
-                                         int status) {
-            Log.i(TAG, "onServicesDiscovered received: " + status);
+        public void onCharacteristicChanged(BluetoothGatt gatt,
+                                            BluetoothGattCharacteristic characteristic) {
+            incomingData = new String(characteristic.getValue());
+            Log.i(TAG,"onCharacteristicChanged  "+incomingData);
+
         }
 
     };
@@ -114,5 +153,8 @@ public class BleConnector {
                 bluetoothGatt = device.connectGatt(appContext, false, gattCallback, BluetoothDevice.TRANSPORT_LE);
             };
         }
+
+        bluetoothGatt.setCharacteristicNotification(serialChar, true);
     }
+
 }
